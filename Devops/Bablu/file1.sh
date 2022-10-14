f1863be90eda564d51b09ed2ebfea257d4667fbf
santoshchanti <santoshmalloju@gmail.com>
2022-10-14 16:49:18 +0530

    Fix: all files

---
+++ Devops/Bablu/file1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Printing text with newline"
+echo -n "Printing text without newline"
+echo -e "\nRemoving \t backslash \t characters\n"
+
+#!/bin/bash
+
+# Add two numeric value
+((sum=25+36))
+
+#Print the result
+echo $sum
+
